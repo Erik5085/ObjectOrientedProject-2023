24806418635434e878bde167cb21e7c4df63433c
Erik5085 <104105546+Erik5085@users.noreply.github.com>
2023-04-23 14:36:04 -0500

    Update Book.java
    constructor for books with restrictions

--- src/libraryProject/Book.java
+++ src/libraryProject/Book.java
@@ -1,45 +1,25 @@
 package libraryProject;
 
-public class Book {
-	private String title;
-	private String author;
-	private int serialNumber;
-	
-	/*
-	 * default constructor
-	 */
-	public Book() {
-		title = "";
-		author = "";
-		serialNumber = 0;
-	}
-	
-	/*
-	 * constructor w/ params
-	 */
-	public Book(String title, String author, int serialNumber) {
-		this.title = title;
-		this.author = author;
-		this.serialNumber = serialNumber;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public int getNum() {
-		return serialNumber;
-	}
-	
-	public void setTitle(String newTitle) {
-		title = newTitle;
-	}
-	public void setAuthor(String newAuthor) {
-		author = newAuthor;
-	}
-	public void setNum(int newSerial) {
-		serialNumber = newSerial;
-	}
+public class Book{
+	public String name;
+    public String author;
+    public int serialNumber;
+    public String genre;
+    private int restriction;
+    public Book(String name,String author,int serialNumber,String genre,int restriction)
+    {
+        this.name=name;
+        this.author=author;
+        this.serialNumber=serialNumber;
+        this.genre=genre;
+        this.restriction=restriction;
+    }
+    public Book(String name,String author,int serialNumber,String genre)
+    {
+        this.name=name;
+        this.author=author;
+        this.serialNumber=serialNumber;
+        this.genre=genre;
+        this.restriction=0;
+    }
 }
