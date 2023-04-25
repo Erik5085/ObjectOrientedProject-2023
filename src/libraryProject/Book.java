253d74755639a3cdd280f888c3edb7fd63ab92bc
Erik5085 <104105546+Erik5085@users.noreply.github.com>
2023-04-24 19:34:32 -0500

    Updated book.java
    Updated to include restricitons

--- src/libraryProject/Book.java
+++ src/libraryProject/Book.java
@@ -5,7 +5,7 @@ public class Book{
     public String author;
     public int serialNumber;
     public String genre;
-    private int restriction;
+    public int restriction;
     public Book(String name,String author,int serialNumber,String genre,int restriction)
     {
         this.name=name;
@@ -21,5 +21,17 @@ public class Book{
         this.serialNumber=serialNumber;
         this.genre=genre;
         this.restriction=0;
+    }
+    public Book()
+    {
+        this.name="";
+        this.author="";
+        this.serialNumber=0;
+        this.genre="";
+        this.restriction=0;
+    }
+    public int get_restriction()
+    {
+        return restriction;
     }
 }
